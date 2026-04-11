a08bce98ebc4ac7041bd7bc379a2f780f9923ed7
DevPaulPOps <perigault.paul@gmail.com>
2026-04-11 23:19:52 +0200

    chore: initial angular 21 setup with tailwind, eslint, husky, ci/cd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,8 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {MessageService} from "primeng/api";
-import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
-    MessageService
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
